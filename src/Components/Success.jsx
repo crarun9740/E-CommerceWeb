69e8d61437f51803e20b3f7065e0a8b985e1ee52
Arun Ramesh Chavan <crarun9740@gmail.com>
2025-06-28 22:36:24 +0530

    success page enhanced

--- src/Components/Success.jsx
+++ src/Components/Success.jsx
@@ -2,9 +2,20 @@ import { Link } from "react-router-dom";
 
 export default function Success() {
   return (
-    <div className="h-screen w-screen flex flex-col overflow-hidden gap-[2rem] items-center justify-center">
-      <h1 className="text-3xl font-bold">Payment Successfull</h1>
-      <Link to={"/"} className="text-xl">
+    <div className="mt-15 flex flex-col overflow-hidden gap-8 items-center justify-center">
+      <h1 className="text-3xl font-bold">Payment Successful</h1>
+
+      <div className="w-[400px] h-[300px]">
+        <img
+          src="/assets/payment-2.gif"
+          alt="Payment Successful"
+          className="w-full h-full"
+        />
+      </div>
+      <Link
+        to="/"
+        className="text-xl border-1 border-gray-300 text-black bg-gray-200 p-3 rounded-2xl hover:bg-gray-300 transition active:border-2"
+      >
         Continue Shopping
       </Link>
     </div>
